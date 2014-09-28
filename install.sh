5d13ef0b5b26481e88aa9d34996ec0ba40c0b2e5
Matt Jordan <mjordan@digium.com>
2014-09-28 08:11:27 -0500

    Add ccache; ssd

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ setup_python() {
 setup_system() {
 	echo "*** Installing System libraries ***"
 
-	PACKAGES="build-essential python-pip vim apache2"
+	PACKAGES="build-essential python-pip vim apache2 ssh ccache"
 	PACKAGES="${PACKAGES} libncurses-dev libssl-dev libxml2-dev libsqlite3-dev uuid-dev uuid"
 	PACKAGES="${PACKAGES} libspandsp-dev binutils-dev libsrtp-dev libedit-dev libjansson-dev"
 	PACKAGES="${PACKAGES} subversion git libxslt1-dev"
